779f3b683a217523b2b9e89f8d58ee592239d528
moriel <morielsh57@gmail.com>
2022-04-18 23:09:36 +0300

    create a map structure for the weather api icons by its number icon api

---
+++ src/shared/consts/icons/weatherIconsMap.ts
@@ -0,0 +1,86 @@
+// import * as icons from "./weatherIconsImports";
+import icon1 from "../../../assets/images/01-s.png";
+import icon2 from "../../../assets/images/02-s.png";
+import icon3 from "../../../assets/images/03-s.png";
+import icon4 from "../../../assets/images/04-s.png";
+import icon5 from "../../../assets/images/05-s.png";
+import icon6 from "../../../assets/images/06-s.png";
+import icon7 from "../../../assets/images/07-s.png";
+import icon8 from "../../../assets/images/08-s.png";
+import icon11 from "../../../assets/images/11-s.png";
+import icon12 from "../../../assets/images/12-s.png";
+import icon13 from "../../../assets/images/13-s.png";
+import icon14 from "../../../assets/images/14-s.png";
+import icon15 from "../../../assets/images/15-s.png";
+import icon16 from "../../../assets/images/16-s.png";
+import icon17 from "../../../assets/images/17-s.png";
+import icon18 from "../../../assets/images/18-s.png";
+import icon19 from "../../../assets/images/19-s.png";
+import icon20 from "../../../assets/images/20-s.png";
+import icon21 from "../../../assets/images/21-s.png";
+import icon22 from "../../../assets/images/22-s.png";
+import icon23 from "../../../assets/images/23-s.png";
+import icon24 from "../../../assets/images/24-s.png";
+import icon25 from "../../../assets/images/25-s.png";
+import icon26 from "../../../assets/images/26-s.png";
+import icon29 from "../../../assets/images/29-s.png";
+import icon30 from "../../../assets/images/30-s.png";
+import icon31 from "../../../assets/images/31-s.png";
+import icon32 from "../../../assets/images/32-s.png";
+import icon33 from "../../../assets/images/33-s.png";
+import icon34 from "../../../assets/images/34-s.png";
+import icon35 from "../../../assets/images/35-s.png";
+import icon36 from "../../../assets/images/36-s.png";
+import icon37 from "../../../assets/images/37-s.png";
+import icon38 from "../../../assets/images/38-s.png";
+import icon39 from "../../../assets/images/39-s.png";
+import icon40 from "../../../assets/images/40-s.png";
+import icon41 from "../../../assets/images/41-s.png";
+import icon42 from "../../../assets/images/42-s.png";
+import icon43 from "../../../assets/images/43-s.png";
+import icon44 from "../../../assets/images/44-s.png";
+
+
+// [icon num api, icon img]
+export const weatherIconsMap = new Map([
+  [1,icon1],
+  [2,icon2],
+  [3,icon3],
+  [4,icon4],
+  [5,icon5],
+  [6,icon6],
+  [7,icon7],
+  [8,icon8],
+  [11,icon11],
+  [12,icon12],
+  [13,icon13],
+  [14,icon14],
+  [15,icon15],
+  [16,icon16],
+  [17,icon17],
+  [18,icon18],
+  [19,icon19],
+  [20,icon20],
+  [21,icon21],
+  [22,icon22],
+  [23,icon23],
+  [24,icon24],
+  [25,icon25],
+  [26,icon26],
+  [29,icon29],
+  [30,icon30],
+  [31,icon31],
+  [32,icon32],
+  [33,icon33],
+  [34,icon34],
+  [35,icon35],
+  [36,icon36],
+  [37,icon37],
+  [38,icon38],
+  [39,icon39],
+  [40,icon40],
+  [41,icon41],
+  [42,icon42],
+  [43,icon43],
+  [44,icon44]
+]);
