bbd6ad021ef56734ab204f8b6f73ae5a78ef8dc7
moriel <morielsh57@gmail.com>
2022-04-23 14:18:39 +0300

    get by default tel aviv location by geoposition

--- src/components/home-page/home.tsx
+++ src/components/home-page/home.tsx
@@ -1,35 +1,41 @@
 import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import WeatherTop from "./weather-top/weatherTop";
 import FiveDaysForecasts from "./five-days-forecasts/fiveDaysForecasts";
 import SearchLocation from "./search-location/searchLocation";
-import { GET_CURRENT_WEATHER_BY_KEY, GET_FIVE_DAYS_FORECASTS } from "../../shared/services/api.service";
+import { GET_CURRENT_WEATHER_BY_KEY, GET_FIVE_DAYS_FORECASTS, GET_LOCATION_BY_GEOPOSITION } from "../../shared/services/api.service";
 import { ICurrentWeatherLocationT, ILocationForecastsT, IWeatherReducerStateT } from "../../shared/reducers/reducer.interfaces";
-import { SET_CURRENT_WEATHER_LOCATION_ACTION, SET_LOCATION_FORECASTS_ACTION } from "../../shared/consts/strings";
-import { ICurrentWeatherLocationApiT, IFiveDailyForecastsApiDailyForecastsT, IFiveDailyForecastsApiT } from "../../shared/consts/weatherApi.interfaces";
+import { TEL_AVIV_GEOPOSITION_LAT, TEL_AVIV_GEOPOSITION_LON } from "../../shared/consts/strings";
+import { ICurrentWeatherLocationApiT, IFiveDailyForecastsApiDailyForecastsT, IFiveDailyForecastsApiT, ILocationGeopositionApiT } from "../../shared/consts/weatherApi.interfaces";
 import { alertMessage } from "../../shared/consts/notification";
+import { useSetLocationForcasts } from "../../shared/hooks/useSetLocationForcasts";
+import { useSetCurrentWeatherLocation } from "../../shared/hooks/useSetCurrentWeatherLocation";
+import { useSetSelectedLocation } from "../../shared/hooks/useSetSelectedLocation";
 
 import "./home.scss";
 import "./homeDark.scss";
 
 const Home: React.FC = (props) => {
-  const dispatch = useDispatch();
   const locationSelected = useSelector((store: IWeatherReducerStateT) => store.locationSelected);
   const locationForecasts = useSelector((store: IWeatherReducerStateT) => store.locationForecasts);
   const isImperialVal = useSelector((store: IWeatherReducerStateT) => store.isImperialVal);
   const isDarkMode = useSelector((store: IWeatherReducerStateT) => store.isDarkMode);
+  const setLocationForcasts = useSetLocationForcasts();
+  const setCurrentWeatherLocation = useSetCurrentWeatherLocation();
+  const setSelectedLocation = useSetSelectedLocation();
 
   useEffect(() => {
-    console.log(isImperialVal);
-    
-    getlocation5Forecasts();
-    getCurrentWeatherByLocation();
+    if(locationSelected){
+      getlocation5Forecasts();
+      getCurrentWeatherByLocation();
+    }
+    else getTelAvivByGeoposition()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [locationSelected, isImperialVal]);
 
   const getlocation5Forecasts = async () => {
     try {
-      const forecastsRes: IFiveDailyForecastsApiT = await GET_FIVE_DAYS_FORECASTS(locationSelected.key, !isImperialVal);
+      const forecastsRes: IFiveDailyForecastsApiT = await GET_FIVE_DAYS_FORECASTS(locationSelected!.key, !isImperialVal);
       const locationForecasts: ILocationForecastsT[] = forecastsRes.DailyForecasts.map((dayItem: IFiveDailyForecastsApiDailyForecastsT) => {
         return {
           EpochDate: dayItem.EpochDate,
@@ -39,7 +45,8 @@ const Home: React.FC = (props) => {
           nightIcon: dayItem.Night.Icon
         }
       });
-      dispatch({ type: SET_LOCATION_FORECASTS_ACTION, locationForecasts: locationForecasts });
+      // dispatch({ type: SET_LOCATION_FORECASTS_ACTION, locationForecasts: locationForecasts });
+      setLocationForcasts(locationForecasts);
     }
     catch (err) {
       alertMessage("There was a problem geting the forcasts", "error");
@@ -50,20 +57,28 @@ const Home: React.FC = (props) => {
 
   const getCurrentWeatherByLocation = async () => {
     try {
-      const currentWeatherRes: ICurrentWeatherLocationApiT = await GET_CURRENT_WEATHER_BY_KEY(locationSelected.key);
+      const currentWeatherRes: ICurrentWeatherLocationApiT = await GET_CURRENT_WEATHER_BY_KEY(locationSelected!.key);
       const currentWeather: ICurrentWeatherLocationT = {
-        cityKey: locationSelected.key,
-        cityName: locationSelected.location,
+        cityKey: locationSelected!.key,
+        cityName: locationSelected!.location,
         weatherText: currentWeatherRes.WeatherText,
         iconNumber: currentWeatherRes.WeatherIcon,
         temperature: isImperialVal ? currentWeatherRes.Temperature.Imperial.Value : currentWeatherRes.Temperature.Metric.Value
       }
-      dispatch({ type: SET_CURRENT_WEATHER_LOCATION_ACTION, currentWeatherLocation: currentWeather });
+      // dispatch({ type: SET_CURRENT_WEATHER_LOCATION_ACTION, currentWeatherLocation: currentWeather });
+      setCurrentWeatherLocation(currentWeather);
     }
     catch (err) {
       alertMessage("There was a problem geting the current weather", "error");
       throw new Error(`There was a problem geting the current weather ${err}`);
     }
+  }
+
+  //get tel-aviv details by geoposition and set the locationSelected global state to tel-aviv details
+  const getTelAvivByGeoposition = async() => {
+    const telAvivData: ILocationGeopositionApiT = await GET_LOCATION_BY_GEOPOSITION(TEL_AVIV_GEOPOSITION_LAT,TEL_AVIV_GEOPOSITION_LON);
+    // dispatch({ type: SET_SELECTED_LOCATION_ACTION, locationSelected: { key: telAvivData.Key, location: telAvivData.AdministrativeArea.LocalizedName } });
+    setSelectedLocation({key: telAvivData.Key, location: telAvivData.AdministrativeArea.LocalizedName});
   }
 
   return (
